5d96217612413322b0eb4a31972b62c27b37c118
Davins <drotar.david@gmail.com>
2022-10-04 22:49:57 +0200

    scraper rewrite

--- autobuyer/ffbuyer.py
+++ autobuyer/ffbuyer.py
@@ -1,5 +1,5 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Firefox()
-driver.get("https://www.komplett.dk/product/1111557/gaming/playstation/playstation-5-ps5?q=playstation%205")
+driver = webdriver.Firefox(executable_path='G:\exam\webdriver\geckodriver.exe')
+driver.get('https://www.komplett.dk/product/1111557/gaming/playstation/playstation-5-ps5?q=playstation%205')
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,66 +1,27 @@
-
+import pandas as pd
+from bs4 import BeautifulSoup
 from selenium import webdriver
 
-from selenium.webdriver.chrome.service import Service
+driver = webdriver.Firefox(executable_path='G:\exam\webdriver\geckodriver.exe')
+driver.get('xxx')
 
-from selenium.webdriver.support.ui import WebDriverWait
+point1 = []
+point2 = []
 
-from selenium.webdriver.support import expected_conditions as EC
+content = driver.page_source
+soup = BeautifulSoup(content)
 
-from bs4 import BeautifulSoup
+for a in soup.findAll(attrs={'class': 'class'}):
+    name = a.find('a')
+    if name not in point1:
+        point1.append(name.text)
 
-import codecs
+for b in soup.findAll(attrs={'class': 'otherclass'}):
+    name2 = b.find('span')
+    point2.append(name.text)
 
-import re
+res1 = pd.Series(point1, name = 'First')
+res2 = pd.Series(point2, name = 'Second')
+df = pd.DataFrame({'First': res1, 'Second': res2})
+df.to_csv('result.csv', index=False, encoding='utf-8')
 
-from webdriver_manager.chrome import ChromeDriverManager
-
-
-driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-
-val = input(“Enter a url: “)
-
-wait = WebDriverWait(driver, 10)
-
-driver.get(val)
-
-get_url = driver.current_url
-
-wait.until(EC.url_to_be(val))
-
-if get_url == val:
-
-    page_source = driver.page_source
-
-
-soup = BeautifulSoup(page_source,features=”html.parser”)
-
-keyword=input(“Enter a keyword to find instances of in the target:”)
-
-matches = soup.body.find_all(string=re.compile(keyword))
-
-len_match = len(matches)
-
-title = soup.title.text
-
-
-file=codecs.open(‘article_scraping.txt’, ‘a+’)
-
-file.write(title+"\n")
-
-file.write("The following are all instances of your keyword:\n")
-
-count=1
-
-for i in matches:
-
-    file.write(str(count) + “.” +  i  + "\n")
-
-    count+=1
-
-file.write(“There were “+str(len_match)+” matches found for the keyword.”
-
-file.close()
-
-driver.quit()
