137bd98e2379d14727e3e06108b998299581d4b9
Davins <drotar.david@gmail.com>
2022-10-04 13:52:05 +0200

    update1

---
+++ autobuyer/ffbuyer.py
@@ -0,0 +1,5 @@
+import time
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+driver.get("https://www.komplett.dk/product/1111557/gaming/playstation/playstation-5-ps5?q=playstation%205")
